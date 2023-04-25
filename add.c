f21782019f8f0ec38b46be9fb3357c844b3d014b
Balaji Gapat <gapatbalaji2901@gmail.com>
2023-04-25 22:14:33 +0530

    adding add.c file

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n1=4;
+    int n2=5;
+    printf("addition of two number : %d",n1+n2);
+    printf("welcome to java\n");
+    printf("welcome to java\n");
+    printf("welcome to java\n");
+    printf("welcome to java\n");
+    printf("welcome to java\n");
+}
